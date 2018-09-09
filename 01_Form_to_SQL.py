e7f00d5c0178ac329a252d3cb86fdfd83485b728
Forcon <art.forcon@gmail.com>
2018-09-09 11:38:59 +0300

    Сделана возможность выбрать либо поиск картинок по ключевому слову (словам), либо по конкретному автору + корректное прохождение по всем страницам для обоих случаев + почищен лишний старый код

--- 01_Form_to_SQL.py
+++ 01_Form_to_SQL.py
@@ -19,11 +19,11 @@ from myOneSheet import *
 Программа для считывания данных с ЯМ и помещения их в SQlite
 Основная программа v 2.3 (с окном ввода)
  """
-
+URL_JM = 'https://www.livemaster.ru/'
 
 def autor_item(url_autor):  # --- Для данных по работам автора
     item_url = []
-    driver.get('https://www.livemaster.ru/' + url_autor)
+    driver.get(URL_JM + url_autor)
 
     try:  # -------- Показываем по 120 картинок на странице
         Select(driver.find_element_by_name('cnt')).select_by_visible_text('120')
@@ -46,7 +46,7 @@ def autor_item(url_autor):  # --- Для данных по работам авт
         try:  # ------- Пролистываем страницу и если это не удается -- завершаем цикл
             driver.find_element_by_link_text(str(ver)).click()
             ver += 1
-        except:
+        except:# =====> Как правильно задать (NoSuchElementException) ?
             iteration = False
 
     return item_url
@@ -54,7 +54,7 @@ def autor_item(url_autor):  # --- Для данных по работам авт
 
 def reseach_item(text_seach):  # ----- Для данных по ключевым словам
     item_url = []
-    driver.get('https://www.livemaster.ru')
+    driver.get(URL_JM)
     # ---- Авторизация на сайте (если нужна) ------
     # element = driver.find_element_by_id("quicklogin").click()
     # driver.find_element_by_name("login").send_keys("art.forcon@gmail.com")
@@ -65,7 +65,6 @@ def reseach_item(text_seach):  # ----- Для данных по ключевым
     elem = driver.find_element_by_name("search")  # Ввели запрос
     elem.send_keys(text_seach)
     driver.find_element_by_class_name("ui-search-btn").submit()
-    # elem.send_keys(Keys.ENTER)
     time.sleep(3)
 
     try:  # -------- Показываем по 120 картинок на странице
@@ -78,14 +77,14 @@ def reseach_item(text_seach):  # ----- Для данных по ключевым
         pass
 
     pagesours = driver.page_source
-    bs = BeautifulSoup(pagesours,
-                       "html.parser")  # , headers = headers) # ---------- Надо отработать вариант с возможными ошибками (например точно делится на 120, или меньше 40 и т.д.)
+    bs = BeautifulSoup(pagesours, "html.parser")  # , headers = headers)
+    # ---------- Надо отработать вариант с возможными ошибками (например точно делится на 120, или меньше 40 и т.д.)
     if bs.find('h1') == 'По данному запросу ничего не найдено':
         messagebox.showinfo("GUI Python", "По этому запросу ничего не найдено, попробуйте его переформулировать")
         exit()
 
     kol_znach = int(re.search(r'\s\d+\s', str(re.search(r'По Вашему запросу.+', bs.text)), flags=0).group())
-    print(f"По Вашему запросу найдено {kol_znach} работ\n")
+    print(f"По Вашему запросу найдено {kol_znach} работ")
 
     iteration = True
     while iteration:
@@ -93,14 +92,15 @@ def reseach_item(text_seach):  # ----- Для данных по ключевым
         bs = BeautifulSoup(pagesours, "html.parser")
 
         item_collection = bs.findAll("a", {"class": "item-block__name"})
-        for el in item_collection:  # ------ Собраем значения с первой страницы
-            if not ('materialy-dlya-tvorchestva' or 'vintazh' or 'dlya-ukrashenij') in str(
-                    el):  # --------- Исключаем матариалы для творчества, винтаж и "для украшений" (и другое тоже добавляем, что не может входить в коллекцию)
+        for el in item_collection:  # ------ Собраем значения
+            # Исключаем матариалы для творчества, винтаж и "для украшений"
+            # (и другое тоже, что не может входить в коллекции)
+            if not ('materialy-dlya-tvorchestva' or 'vintazh' or 'dlya-ukrashenij') in str(el):
                 item_url.append(str(el).split('href="')[1].split('" title')[0])
 
         try:  # ------- Пролистываем страницу и если это не удается -- завершаем цикл
             driver.find_element_by_class_name("pagebar__arrow--right").click()
-        except:
+        except: # =====> Как правильно задать (NoSuchElementException) ?
             iteration = False
             pass
 
@@ -123,128 +123,15 @@ rez_vibor = TextSearсh(root).sendValue  # Получаем текст для д
 text_seach = rez_vibor[1] if rez_vibor[1] != '' else reseach_item(rez_vibor[0])
 item_url_spisok = autor_item(text_seach) if rez_vibor[1] != '' else reseach_item(text_seach)
 
-# text_seach = 'выдра'
-# item_url_spisok = reseach_item(text_seach)
-
-# if rez_vibor[1] != '':
-#     autor = rez_vibor[1]
-# else:
-#     autor = ''
-#     text_seach = rez_vibor[0]
-
-
-# item_url = []
-# headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
-#                          'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
-
-# --------- Формирует в базе данные по запросу ключевого слова
-# url_list = 'https://www.livemaster.ru/search.php?action=paging&searchtype=1&thw=0&from='
-# url_autor = 'https://www.livemaster.ru/set-bs'
-
-
-# if autor == '':
-#     # driver.get('https://www.livemaster.ru')
-#     # element = driver.find_element_by_id("quicklogin").click()
-#     #
-#     # # ---- Авторизация на сайте ------
-#     # # driver.find_element_by_name("login").send_keys("art.forcon@gmail.com")
-#     # # driver.find_element_by_name("password").send_keys("1970Fortuna")
-#     # # driver.find_element_by_name("password").send_keys(Keys.ENTER)
-#     # #
-#     # # time.sleep(1)
-#     #
-#     # elem = driver.find_element_by_name("search")
-#     # elem.send_keys(text_seach)
-#     # driver.find_element_by_class_name("ui-search-btn").submit()
-#     # # elem.send_keys(Keys.ENTER)
-#     # time.sleep(3)
-# else:
-#     driver.get(url_autor)
-
-# -------- Показываем по 120 картинок на странице
-# if autor == '':  # ----- Для данных по ключевым словам
-#     try:
-#         driver.find_element_by_id("perPage").click()
-#         select = driver.find_elements_by_class_name("js-perpage-btn")
-#         for option in select:
-#             if option.get_attribute('data-value') == '6':
-#                 option.click()
-#     except:
-#         pass
-# else:
-#     try:
-#         Select(driver.find_element_by_name('cnt')).select_by_visible_text('120')
-#         pass
-#     except:
-#         pass
-
-#
-# iteration = True
-# # ------ Отсюда начинается сбор данных о работах
-#
-# while iteration:
-#     if autor == '':  # ----- Для данных по ключевым словам
-#         pagesours = driver.page_source
-#         bs = BeautifulSoup(pagesours,
-#                            "html.parser")  # , headers = headers) # ---------- Надо отработать вариант с возможными ошибками (например точно делится на 120, или меньше 40 и т.д.)
-#         if bs.find('h1') == 'По данному запросу ничего не найдено':
-#
-#         kol_znach = int(re.search(r'\s\d+\s', str(re.search(r'По Вашему запросу.+', bs.text)), flags=0).group())
-#         print(f"По Вашему запросу найдено {kol_znach} работ\n")
-#
-#         bs = BeautifulSoup(pagesours, "html.parser")
-#
-#         item_collection = bs.findAll("a", {"class": "item-block__name"})
-#         for el in item_collection:  # ------ Собраем значения с первой страницы
-#             if not ('materialy-dlya-tvorchestva' or 'vintazh' or 'dlya-ukrashenij') in str(
-#                     el):  # --------- Исключаем матариалы для творчества, винтаж и "для украшений" (и другое тоже добавляем, что не может входить в коллекцию)
-#                 item_url.append(str(el).split('href="')[1].split('" title')[0])
-#
-#         try:  # ------- Пролистываем страницу
-#             driver.find_element_by_class_name("pagebar__arrow--right").click()
-#         except:
-#             iteration = False
-#             pass
-
-#
-#
-# while iteration:
-# else: # --- Для данных по работам автора
-#     item_collection = bs.findAll("a", { "class" : "js-stat-main-item-title" })
-#
-#     bs = BeautifulSoup(pagesours, "html.parser")
-#     item_collection = bs.findAll("a", {"class": "item-block__name"})
-#
-#     for el in item_collection: # ------ Собраем значения с первой страницы
-#         if not ('materialy-dlya-tvorchestva'  or 'vintazh' or 'dlya-ukrashenij') in str(el):# --------- Исключаем матариалы для творчества, винтаж и "для украшений" (и другое тоже добавляем, что не может входить в коллекцию)
-#             item_url.append(str(el).split('href="')[1].split('"')[0])
-#
-#     try:# ------- Пролистываем первую страницу
-#         driver.find_element_by_class_name("pagebar__page").send_keys(Keys.ENTER)
-#     except:
-#         iteration = False
-#         pass
-
-# breakpoint()
-# # --------- Собираем все значения ссылок на работы, начиная со второй страницы (пока без пролистывания по автору)
-# for i in range(1, int(kol_znach/120)+1):
-#     driver.get(url_list + str(120*i))
-#     pagesours = driver.page_source
-#     bs = BeautifulSoup(pagesours, "html.parser")
-#     item_collection = bs.findAll("a", {"class": "item-block__name"})
-#
-#     for el in item_collection:
-#         if not ('materialy-dlya-tvorchestva' or 'vintazh' or 'dlya-ukrashenij') in str(el): # --------- Исключаем матариалы для творчества (можно и другие тоже добавить)
-#             item_url.append(str(el).split('href="')[1].split('" title')[0])
-
 # ----- Проходим по каждой странице, собраем данные, записываем в базу
+print(f"В базу будет внесено {len(item_url_spisok)} работ\n")
 for k, el in enumerate(item_url_spisok):
-    name_url = 'https://www.livemaster.ru/' + el
+    name_url = URL_JM + el
     baze = one_list(text_seach, name_url)
 
     try:
         cursor.execute("""INSERT INTO 'Items' (
-        'Autor', 'Url_Autor', 'Url_Item', 'Favor', 'Gallery', 'Tags', 'Word_Search', 'Price', 'Name_Img', 'Material', 'Size', 'Location') 
+            'Autor', 'Url_Autor', 'Url_Item', 'Favor', 'Gallery', 'Tags', 'Word_Search', 'Price', 'Name_Img', 'Material', 'Size', 'Location') 
             VALUES ('{:s}', '{:s}', '{:s}', '{:}', '{:}', '{:}', '{:s}', '{:}', '{:s}', '{:s}', '{:s}', '{:s}')
             """.format(baze[0], baze[1], baze[2], baze[3], baze[4], baze[5], baze[6], baze[7], baze[8],
                        baze[9], baze[10], baze[11]))
--- myRoot_text.py
+++ myRoot_text.py
@@ -6,6 +6,7 @@ from myBoolean import *
 ------ Эксперименты с окнами -------
 для сбора картинок и информации про работу в базу
 """
+URL_JM = 'https://www.livemaster.ru/'
 
 # класс главного окна
 class TextSearсh:
@@ -32,21 +33,25 @@ class TextSearсh:
 
         self.text_entry2 = Entry(self.master, textvariable = self.message2)
         self.text_entry2.place(relx=.5, y = 115, anchor="c", width=300)
-        self.text_entry2.insert(END, 'https://www.livemaster.ru/')
+        self.text_entry2.insert(END, URL_JM)
         self.message_button2 = Button(self.master, text = "Работы автора", command = self.text_autor)
         self.message_button2.place(x = 485, y = 115, anchor="w", width = 150)
 
         self.master.protocol('WM_DELETE_WINDOW', self.exitMethod)
         self.master.mainloop()
 
+
     def __str__(self):
         return self.sendValue
+
 
     def text_item(self):
         self.show_message(1)
 
+
     def text_autor(self):
         self.show_message(2)
+
 
     def show_message(self, ver):  # ----- Смотрит, введен ли текст и выдает предупреждение
         if self.text_entry1.get() == '' and ver == 1:
@@ -56,7 +61,7 @@ class TextSearсh:
             self.dialog = YesNo(self.master)
             self.returnValue = self.dialog.go('Вопрос:', 'В запросе латинские буквы. Вы уверены?')
             if self.returnValue:
-                self.sendValue = (self.text_entry1.get(), self.text_entry2.get()[len('https://www.livemaster.ru/'):])
+                self.sendValue = (self.text_entry1.get(), self.text_entry2.get()[len(URL_JM):])
                 # self.text_entry1.delete('0', END)
                 self.master.destroy()
             else:
@@ -66,26 +71,19 @@ class TextSearсh:
                 # self.master.focus_set()
                 self.text_entry1.focus_set()
 
-        elif self.text_entry2.get()[len('https://www.livemaster.ru/'):] == '' and ver == 2:
+        elif self.text_entry2.get()[len(URL_JM):] == '' and ver == 2:
             messagebox.showinfo("GUI Python", "Надо ввести текст для поиска")
             self.text_entry2.focus_set()
         elif ver == 2 and re.search(r"[а-яА-ЯёЁ]", str(self.text_entry2.get())):
             messagebox.showinfo("GUI Python", "В адресе не может быть русских букв")
-            self.text_entry2.delete(len('https://www.livemaster.ru/'), END)
+            self.text_entry2.delete(len(URL_JM), END)
             self.text_entry2.focus_set()
 
         else:
-            self.sendValue = (self.text_entry1.get(), self.text_entry2.get()[len('https://www.livemaster.ru/'):])
+            self.sendValue = (self.text_entry1.get(), self.text_entry2.get()[len(URL_JM):])
             # self.text_entry.delete('0', END)
             self.master.destroy()
 
-    # def openDialog(self):
-    #     # self.dialog = child(self.master)
-    #     self.sendValue = self.text_entry.get()
-    #     self.returnValue = self.dialog.go(self.sendValue)
-    #     if self.returnValue:
-    #         self.text_entry.delete('0', END)
-    #         self.text_entry.insert('0', self.returnValue)
 
     def exitMethod(self):
         self.dialog = YesNo(self.master)
@@ -93,105 +91,11 @@ class TextSearсh:
         if self.returnValue:
             self.master.destroy()
 
-# # класс дочернего окна
-# class child:
-#     def __init__(self, master):
-#         self.slave = Toplevel(master)
-#         self.slave.title('child')
-#         # self.slave.geometry('200x150+500+375')
-#         self.slave.geometry(Center_widows(200, 150))
-#         self.frame = Frame(self.slave)
-#         self.frame.pack(side=BOTTOM)
-#         self.accept_button = Button(self.frame, text='accept', command=self.accept)
-#         self.accept_button.pack(side=LEFT)
-#         self.cancel_button = Button(self.frame, text='cancel', command=self.cancel)
-#         self.cancel_button.pack(side=RIGHT)
-#         self.text = Text(self.slave, background='white')
-#         self.text.pack(side=TOP, fill=BOTH, expand=YES)
-#         self.slave.protocol('WM_DELETE_WINDOW', self.cancel)
-#
-#     def go(self, myText=''):
-#         self.text.insert('0.0', myText)
-#         self.newValue = None
-#         self.slave.grab_set()
-#         self.slave.focus_set()
-#         self.slave.wait_window()
-#         return self.newValue
-#
-#     def accept(self):
-#         self.newValue = self.text.get('0.0', END)
-#         self.slave.destroy()
-#
-#     def cancel(self):
-#         self.slave.destroy()
 
-# # класс диалогового окна выхода
-# class yesno:
-#     def __init__(self, master):
-#         self.slave = Toplevel(master)
-#         self.slave.title('exit dialog')
-#         # self.slave.geometry('200x100+300+250')
-#         self.slave.geometry(Center_widows(200, 100))
-#         self.frame = Frame(self.slave)
-#         self.frame.pack(side=BOTTOM)
-#         self.yes_button = Button(self.frame, text='yes', command=self.yes)
-#         self.yes_button.pack(side=LEFT)
-#         self.no_button = Button(self.frame, text='no', command=self.no)
-#         self.no_button.pack(side=RIGHT)
-#         self.label = Label(self.slave)
-#         self.label.pack(side=TOP, fill=BOTH, expand=YES)
-#         self.slave.protocol('WM_DELETE_WINDOW', self.no)
-#
-#     def go(self, title='', message=''):
-#         self.slave.title(title)
-#         self.label.configure(text=message)
-#         self.booleanValue = TRUE
-#         self.slave.grab_set()
-#         self.slave.focus_set()
-#         self.slave.wait_window()
-#         return self.booleanValue
-#
-#     def yes(self):
-#         self.booleanValue = TRUE
-#         self.slave.destroy()
-#
-#     def no(self):
-#         self.booleanValue = FALSE
-#         self.slave.destroy()
-
-# # Располагает окно по центру страницы
-# class Center_widows(object): # ----- Располагает окна по центру экрана
-#     def __init__(self, width, height):
-#         self.width = width
-#         self.height = height
-#
-#     def __str__(self):
-#         return str(self.width) + "x" + str(self.height) + "+" + str(
-#             (root.winfo_screenwidth() - self.width) // 2) + "+" + str((root.winfo_screenheight() - self.height) // 2)
-
-# создание окна
-
-# class ItemPozition:
-#     def __init__(self, message = '[имя поля]', mes_btn = '[кнопка]', lbl_y = 0, ent_y = 0, url = ''):
-#         self.name_label = Label(text='message')
-#         self.name_label.place(relx=.5, y = lbl_y, anchor="c")
-#
-#         self.text_entry = Entry(textvariable = self.message)
-#         self.text_entry.place(relx=.5, y = ent_y, anchor="c", width=300)
-#         self.text_entry.focus_set()
-#         self.text_entry.insert(END, url)
-#         self.message_button = Button(text = mes_btn, command = self.show_message)
-#         self.message_button.place(x = 485, y = ent_y, anchor="w", width = 150)
-
-# запуск окна
 if __name__ == '__main__':
     try:
         root = Tk()
         text = TextSearсh(root).sendValue
         print(text)
-        # root.destroy()
-    except(AttributeError):
+    except (AttributeError):
         pass
-
-
-# print(sendValue)
